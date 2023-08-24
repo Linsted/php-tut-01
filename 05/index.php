ff71a82a5999d99f5ce1290bb7fbb277d9e0058e
Linsted <ivan.malenko18@gmail.com>
2023-08-24 20:53:24 +0300

    feat: tut 5

--- 05/index.php
+++ 05/index.php
@@ -1,2 +1,19 @@
 <?php 
 error_reporting(-1);
+
+// 1st method to define constant variable.
+define("CONST_1", "value 1");
+
+echo CONST_1;
+
+// 2nd method to define constant variable.
+const CONST_2 = 'value 2';
+
+var_dump(CONST_2);
+
+const CONST_2 = 'new value';
+
+// ERR- const is already defined
+var_dump(CONST_2);
+
+
